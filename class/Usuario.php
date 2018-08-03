774dcf41b9db70837ddc0675dac070ae2e14ad53
Danilo Rodrigues <danilo_russini@hotmail.com>
2018-08-03 15:26:39 -0300

    PDO Select finalizado

--- class/Usuario.php
+++ class/Usuario.php
@@ -13,15 +13,15 @@ class Usuario {
 	}
 
 	public function setIdusuario($value){
-		$this->idusuario = $value
+		$this->idusuario = $value;
 	}
 
-		public function getDeslogin(){
+	public function getDeslogin(){
 		return $this->deslogin;
 	}
 
 	public function setDeslogin($value){
-		$this->deslogin = $value
+		$this->deslogin = $value;
 	}
 
 	public function getDessenha(){
@@ -29,7 +29,7 @@ class Usuario {
 	}
 
 	public function setDessenha($value){
-		$this->dessenha = $value
+		$this->dessenha = $value;
 	}
 
 	public function getDtcadastro(){
@@ -37,7 +37,7 @@ class Usuario {
 	}
 
 	public function setDtcadastro($value){
-		$this->dtcadastro = $value
+		$this->dtcadastro = $value;
 	}
 
 	public function loadById($id){
@@ -47,11 +47,27 @@ class Usuario {
 			":ID"=>$id
 		));
 
-		if (count($results[0]) > 0){
+		if (count($result) > 0){
 
-			
+			$row = $result[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
 		}
 
+	}
+
+	public function __toString(){
+
+		return json_encode(array(
+			"idusuario"=>$this->getIdusuario(),
+			"deslogin"=>$this->getDeslogin(),
+			"dessenha"=>$this->getDessenha(),
+			"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+
+		));
 	}
 }
 
